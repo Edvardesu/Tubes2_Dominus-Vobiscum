9a89232204df393c644fc11faf1f954dbf461b93
Bryan Cornelius Lauwrence <bryan.cornelius.lauw@gmail.com>
2024-04-21 17:24:28 +0700

    scrap link di body

---
+++ src/algorithm/tes.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	var sliceURL []string
+	scrapeUrl := "https://en.wikipedia.org/wiki/English_Wikipedia"
+	// Instantiate a new collector
+	c := colly.NewCollector()
+	// c := colly.NewCollector(colly.AllowedDomains("https://en.wikipedia.org/wiki/"))
+
+	// Visit the website and print its title
+	c.OnHTML("p a[href]", func(e *colly.HTMLElement) {
+		if strings.HasPrefix(e.Attr("href"), "/wiki/") {
+			sliceURL = append(sliceURL, e.Attr("href"))
+		}
+	})
+
+	// Start scraping
+	c.Visit(scrapeUrl)
+
+	for i:=0; i < len(sliceURL); i++ {
+		fmt.Println(sliceURL[i])
+	}
+}
