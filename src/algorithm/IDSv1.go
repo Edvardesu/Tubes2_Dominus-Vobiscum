487dd05879e05e275152491c4b586d965d14106a
VansonK <vansonkurnialim@gmail.com>
2024-04-24 23:08:49 +0700

    IDS versi mantep
    pake caching, cek title, masih single path

--- src/algorithm/IDSv1.go
+++ src/algorithm/IDSv1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"sync"
 	"time"
 
@@ -21,8 +22,8 @@ func main() {
 	var wg sync.WaitGroup
 
 	// Create a new collector
-	c := colly.NewCollector(colly.AllowedDomains("en.wikipedia.org"))
-	start = "Jokowi"
+	c := colly.NewCollector(colly.AllowedDomains("en.wikipedia.org"), colly.CacheDir("./Cache"))
+	start = "Joko_Widodo"
 	destination = "Cadmium"
 
 	central(c, &wg)
@@ -45,12 +46,17 @@ func central(c *colly.Collector, wg *sync.WaitGroup) {
 		path_of_url = append(path_of_url, start)
 
 		for {
+
 			if len(path_found) > 0 {
 				end := time.Now()
 				fmt.Println("Path found : ", path_found)
 				fmt.Println("Number of links visited : ", total_link_visited)
 				fmt.Println("Path length : ", len(path_found[0]))
 				fmt.Println("Runtime : ", end.Sub(begin))
+				break
+			}
+			if iterasi == 5 {
+				fmt.Println("Sengaja dibatesin 5, coba tanya Vanson")
 				break
 			}
 			fmt.Print("No result found with depth ", iterasi, "! Attempting with depth ", iterasi+1, "...\n\n")
@@ -61,13 +67,13 @@ func central(c *colly.Collector, wg *sync.WaitGroup) {
 			fmt.Println("Depth ", iterasi)
 
 			wg.Add(1)
-			dls(path_of_url, start, iterasi, wg)
+			dls(c, path_of_url, start, iterasi, wg)
 			wg.Wait()
 		}
 	}
 }
 
-func dls(path_of_url []string, url_scraped string, iterasi int, wg *sync.WaitGroup) {
+func dls(c *colly.Collector, path_of_url []string, url_scraped string, iterasi int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	// fmt.Println("IN")
 
@@ -77,32 +83,53 @@ func dls(path_of_url []string, url_scraped string, iterasi int, wg *sync.WaitGro
 	list_of_url := make([]string, 0)
 	var link_to_visit string = page + url_scraped
 
-	validasiLinkIDS(&link_to_visit, &list_of_url)
+	new_path_of_url := make([]string, 0)
+	new_path_of_url = append(new_path_of_url, path_of_url...)
+	validasiLinkIDS(&link_to_visit, &list_of_url, new_path_of_url)
+	// fmt.Println(list_of_url)
 	// fmt.Println("OUT")
 	iterasi -= 1
 	for j := 0; j < len(list_of_url); j++ {
-		new_path_of_url := make([]string, 0)
-		new_path_of_url = append(new_path_of_url, path_of_url...)
-		new_path_of_url = append(new_path_of_url, list_of_url[j])
 
 		if list_of_url[j] == destination {
+			new_path_of_url := make([]string, 0)
+			new_path_of_url = append(new_path_of_url, path_of_url...)
+			new_path_of_url = append(new_path_of_url, list_of_url[j])
 			path_found = append(path_found, new_path_of_url)
 
 			// print duration
-			// end := time.Now()
-			// fmt.Println("Path found : ", path_found[0])
-			// fmt.Println("Number of links visited : ", total_link_visited)
-			// fmt.Println("Path length : ", len(path_found[0]))
-			// fmt.Println("Runtime : ", end.Sub(begin))
-			// os.Exit(0)
-
-		} else {
-			if iterasi > 0 {
-				wg.Add(1)
-				go func() {
-					dls(new_path_of_url, list_of_url[j], iterasi, wg)
-				}()
-			}
+			end := time.Now()
+			fmt.Println("Path found : ", path_found[0])
+			fmt.Println("Number of links visited : ", total_link_visited)
+			fmt.Println("Path length : ", len(path_found[0]))
+			fmt.Println("Runtime : ", end.Sub(begin))
+			os.Exit(0)
+		}
+		if iterasi > 0 {
+			new_path_of_url := make([]string, 0)
+			new_path_of_url = append(new_path_of_url, path_of_url...)
+			new_path_of_url = append(new_path_of_url, list_of_url[j])
+			wg.Add(1)
+			go func() {
+				dls(c, new_path_of_url, list_of_url[j], iterasi, wg)
+			}()
 		}
 	}
+	// for j := 0; j < len(list_of_url); j++ {
+	// 	title := getValidLink(list_of_url[j])
+	// 	if title == destination {
+	// 		new_path_of_url := make([]string, 0)
+	// 		new_path_of_url = append(new_path_of_url, path_of_url...)
+	// 		new_path_of_url = append(new_path_of_url, list_of_url[j])
+	// 		path_found = append(path_found, new_path_of_url)
+
+	// 		// print duration
+	// 		end := time.Now()
+	// 		fmt.Println("Path found : ", path_found[0])
+	// 		fmt.Println("Number of links visited : ", total_link_visited)
+	// 		fmt.Println("Path length : ", len(path_found[0]))
+	// 		fmt.Println("Runtime : ", end.Sub(begin))
+	// 		os.Exit(0)
+	// 	}
+	// }
 }
--- src/algorithm/scrap.go
+++ src/algorithm/scrap.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -29,23 +31,58 @@ func checkHastag(l string) int {
 func cutLink(l string, i int) string {
 	return l[:i]
 }
-func getValidLink(link string) string {
-	c := colly.NewCollector(colly.AllowedDomains("en.wikipedia.org"))
-	var result string = "test"
-	c.OnResponse(func(r *colly.Response) {
-		// result = r.Request.URL
-		fmt.Println("link visited :", r.Request.URL)
-	})
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println(r.Request.URL, " scraped!")
-	})
-	c.Visit("https://en.wikipedia.org/wiki/" + link)
+
+func addUnderScore(title string) string {
+	var result string
+	for i := 0; i < len(title); i++ {
+		if title[i] == ' ' {
+			result += "_"
+		} else {
+			result += string(title[i])
+		}
+	}
 	return result
 }
-func validasiLinkIDS(l *string, slice *[]string) {
-	c := colly.NewCollector(colly.AllowedDomains("en.wikipedia.org"))
 
+func getValidLink(link string) string {
+	c := colly.NewCollector(colly.AllowedDomains("en.wikipedia.org"))
+	var title string
+	c.OnHTML(".firstHeading", func(e *colly.HTMLElement) {
+		// Extract the title of the Wikipedia page
+		title = strings.ToLower(e.Text)
+		// title = addUnderScore(title)
+		title = strings.ReplaceAll(title, " ", "_")
+		if title == destination {
+
+		}
+	})
+	c.Visit("https://en.wikipedia.org/wiki/" + link)
+	return title
+}
+func validasiLinkIDS(l *string, slice *[]string, new_path_of_url []string) {
+	c := colly.NewCollector(colly.AllowedDomains("en.wikipedia.org"), colly.CacheDir("./Cache"))
+
+	// checking title
+	var title string
+	c.OnHTML(".firstHeading", func(e *colly.HTMLElement) {
+		// Extract the title of the Wikipedia page
+		// title = strings.ToLower(e.Text)
+		// title = addUnderScore(title)
+		title = strings.ReplaceAll(e.Text, " ", "_")
+		if title == destination {
+			new_path_of_url = append(new_path_of_url, title)
+			path_found = append(path_found, new_path_of_url)
+
+			end := time.Now()
+			fmt.Println("Path found : ", path_found[0])
+			fmt.Println("Number of links visited : ", total_link_visited)
+			fmt.Println("Path length : ", len(path_found[0]))
+			fmt.Println("Runtime : ", end.Sub(begin))
+			os.Exit(0)
+		}
+	})
 	// fmt.Println(*l)
+	// scraping
 	c.OnHTML("p a[href^='/wiki']", func(e *colly.HTMLElement) {
 		total_link_visited += 1
 		link := e.Attr("href")[6:]
@@ -58,7 +95,9 @@ func validasiLinkIDS(l *string, slice *[]string) {
 
 			// cek apakah link ada di array URL
 			if !isIn(*slice, link) {
-				// link = getValidLink(link)
+				// title := getValidLink(link)
+				// fmt.Println("Title:", title)
+
 				*slice = append(*slice, link)
 				// fmt.Println("Link found :", link)
 			}
@@ -68,25 +107,3 @@ func validasiLinkIDS(l *string, slice *[]string) {
 	// Start scraping
 	c.Visit(*l)
 }
-
-// func main() {
-// 	var sliceURL []string
-// 	scrapeUrl := "https://en.wikipedia.org/wiki/Frikadelle"
-// 	// Instantiate a new collector
-// 	c := colly.NewCollector(colly.AllowedDomains("en.wikipedia.org"))
-
-// 	c.OnRequest(func(e *colly.Request) {
-// 		fmt.Println("Mengunjungi ", e.URL)
-// 	})
-
-// 	c.OnError(func(e *colly.Response, err error) {
-// 		log.Println("Terjadi error: ", err)
-// 	})
-
-// 	// Visit the website and print its title
-// 	validasiLink(&scrapeUrl, c, &sliceURL)
-
-// 	for i := 0; i < len(sliceURL); i++ {
-// 		fmt.Println(sliceURL[i])
-// 	}
-// }
